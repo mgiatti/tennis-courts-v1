eb3438d79f37fddd4f2cd35046b089d3ab1b4da7
Marcelo Giatti <mb.giatti@gmail.com>
2021-11-04 15:20:21 -0300

    Add more tests related to refund

--- src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
+++ src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
@@ -26,11 +26,42 @@ public class ReservationServiceTest {
     @Test
     public void getRefundValueFullRefund() {
         Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusDays(2));
+        Assert.assertEquals(new BigDecimal(10), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
+    }
 
-        LocalDateTime startDateTime = LocalDateTime.now().plusDays(2);
+    @Test
+    public void getRefundValue75PercentRefund() {
+        Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(23).plusMinutes(59));
+        Assert.assertEquals(new BigDecimal(7.50).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
 
-        schedule.setStartDateTime(startDateTime);
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(13).plusMinutes(40));
+        Assert.assertEquals(new BigDecimal(7.50).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
 
-        Assert.assertEquals(reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()), new BigDecimal(10));
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(12));
+        Assert.assertEquals(new BigDecimal(7.50).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
+
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(11).plusMinutes(59));
+        Assert.assertNotEquals(new BigDecimal(7.50).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
+
+    }
+
+    @Test
+    public void getRefundValue50PercentRefund() {
+        Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(10));
+        Assert.assertEquals(new BigDecimal(5.0).setScale(1), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
+    }
+
+    @Test
+    public void getRefundValue25PercentRefund() {
+        Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(1));
+        Assert.assertEquals(new BigDecimal(2.5).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
+
+        schedule.setStartDateTime(LocalDateTime.now().plusMinutes(1));
+        Assert.assertEquals(new BigDecimal(2.5).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()));
+
     }
 }
