0531c50e7e763782ad54be669185e861c7daf53f
Marcelo Giatti <mb.giatti@gmail.com>
2021-11-04 15:19:27 -0300

    Populate more schedules and guests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,14 @@
 insert into guest(id, name) values(null, 'Roger Federer');
 insert into guest(id, name) values(null, 'Rafael Nadal');
+insert into guest(id, name) values(null, 'Marcelo Giatti');
+
 
 insert into tennis_court(id, name) values(null, 'Roland Garros - Court Philippe-Chatrier');
+insert into tennis_court(id, name) values(null, 'My private Court');
 
-insert
-    into
-        schedule
-        (id, start_date_time, end_date_time, tennis_court_id)
-    values
-        (null, '2020-12-20T20:00:00.0', '2020-02-20T21:00:00.0', 1);
+insert into schedule(id, start_date_time, end_date_time, tennis_court_id) values(null, '2021-11-05T20:00:00.0', '2021-11-05T21:00:00.0', 1);
+
+insert into schedule(id, start_date_time, end_date_time, tennis_court_id) values(null, '2021-11-05T10:00:00.0', '2021-11-05T11:00:00.0', 2);
+
+insert into schedule(id, start_date_time, end_date_time, tennis_court_id) values(null, '2021-11-04T20:00:00.0', '2021-11-04T21:00:00.0', 2);
+
